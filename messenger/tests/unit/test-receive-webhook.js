376909534eed79083e8e709b4b8a8cb491b02515
p0oker <p0oker@protonmail.com>
2020-12-22 00:42:39 +0800

    add basic tests for receive webhook

---
+++ messenger/tests/unit/test-receive-webhook.js
@@ -0,0 +1,64 @@
+"use strict";
+
+const app = require("../../app.js");
+const chai = require("chai");
+const expect = chai.expect;
+let event, context;
+
+// payload is taken from https://developers.facebook.com/docs/messenger-platform/reference/webhook-events/messages
+const mockMessagePayload = {
+  sender: {
+    id: "<PSID>",
+  },
+  recipient: {
+    id: "<PAGE_ID>",
+  },
+  timestamp: 1458692752478,
+  message: {
+    mid: "mid.1457764197618:41d102a3e1ae206a38",
+    text: "hello, world!",
+    quick_reply: {
+      payload: "<DEVELOPER_DEFINED_PAYLOAD>",
+    },
+  },
+};
+
+describe("Tests for receiveWebhookHandler", function () {
+  it("should return 404 if message is not coming from a page", async () => {
+    let event = {
+      body: {
+        object: "NOT page",
+        entry: [
+          {
+            messaging: [
+              {
+                ...mockMessagePayload,
+              },
+            ],
+          },
+        ],
+      },
+    };
+    const result = await app.receiveWebhookHandler(event, context);
+    expect(result.statusCode).to.be.equal(404);
+  });
+
+  it("should return 200 to all requests coming from the page", async () => {
+    let event = {
+      body: {
+        object: "page",
+        entry: [
+          {
+            messaging: [
+              {
+                ...mockMessagePayload,
+              },
+            ],
+          },
+        ],
+      },
+    };
+    const result = await app.receiveWebhookHandler(event, context);
+    expect(result.statusCode).to.be.equal(200);
+  });
+});
